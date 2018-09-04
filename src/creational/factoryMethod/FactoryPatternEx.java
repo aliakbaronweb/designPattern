5d5b1afd98f1891f26be22df17388a70dce3f429
Ali Akbar <ali.zahra.nice@gmail.com>
2018-09-04 22:59:11 +0430

    add description to factory method design pattern

--- src/creational/factoryMethod/FactoryPatternEx.java
+++ src/creational/factoryMethod/FactoryPatternEx.java
@@ -2,6 +2,17 @@ package creational.factoryMethod;
 
 public class FactoryPatternEx {
 
+    // GoF Definition: Define an interface for creating an object, but let subclasses decide which class to instantiate.
+    // The factory method lets a class defer instantiation to subclasses.
+
+    // Concept: The concept can be best described with the examples below.
+
+    // Real-Life Example:
+    // Suppose you have two different types of televisions—one with an LED screen and another with an LCD screen.
+    // If any of these starts malfunctioning, you will call a TV repairman to request a visit to your residence.
+    // The repairman must ask first what kind of TV is nonoperational.
+    // As per your input, he’ll carry the required instruments with him.
+
     public static void main(String[] args) throws Exception {
         System.out.println("***Factory Pattern Demo***\n");
         IAnimalFactory animalFactory = new ConcreteFactory();
